4432bd7bac90f58dfe913dd2718be8155b27037e
Janesh7 <janeshwalia273@gmail.com>
2023-04-24 11:36:09 +0530

    Hero and get started

--- src/components/GetStarted.jsx
+++ src/components/GetStarted.jsx
@@ -2,7 +2,9 @@ import styles from "../style";
 import { arrowUp } from "../assets";
 
 const GetStarted = () => (
+  // Blue gradeint circle of req w and h , padding of 2px ,cursor-pointer (clicakable)
   <div className={`${styles.flexCenter} w-[140px] h-[140px] rounded-full bg-blue-gradient p-[2px] cursor-pointer`}>
+    {/* Creating an element inside it with the background color, so now there is basically the padding left , ie a 2px circle ring of blue gradient */}
     <div className={`${styles.flexCenter} flex-col bg-primary w-[100%] h-[100%] rounded-full`}>
       <div className={`${styles.flexStart} flex-row`}>
         <p className="font-poppins font-medium text-[18px] leading-[23.4px]">
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -16,9 +16,11 @@ const Hero = () => {
 
         <div className="flex flex-row justify-between items-center w-full">
           <h1 className="flex-1 font-poppins font-semibold ss:text-[72px] text-[52px] text-white ss:leading-[100.8px] leading-[75px]">
+            {/* ss- for stroke, ss:leading- heigth btw the lines */}
             The Next <br className="sm:block hidden" />{" "}
             <span className="text-gradient">Generation</span>{" "}
           </h1>
+          {/* Get started circle */}
           <div className="ss:flex hidden md:mr-4 mr-0">
             <GetStarted />
           </div>
@@ -28,6 +30,7 @@ const Hero = () => {
           Payment Method.
         </h1>
         <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+          {/* Max width and amrgin top */}
           Our team of experts uses a methodology to identify the credit cards
           most likely to fit your needs. We examine annual percentage rates,
           annual fees.
